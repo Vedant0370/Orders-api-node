fad05353207ac6a66521d765e9deda079f04cf27
Vedant0370 <vedant.0370@gmail.com>
2023-07-21 22:19:06 +0530

    Final chnages 1

--- index.js
+++ index.js
@@ -1,20 +1,72 @@
 
 
-// const express = require("express")
-// const app = express()
-// port = 4000
+// // const express = require("express")
+// // const app = express()
+// // port = 4000
 
-// app.get("/" , (req,res) => {
+// // app.get("/" , (req,res) => {
 
-//     res.send("hello Vedant")
-// })
-// app.listen(port, () => {
+// //     res.send("hello Vedant")
+// // })
+// // app.listen(port, () => {
 
-//     console.log(`the server is ruuning on http://127.0.0.1:${port}`)
-// })
+// //     console.log(`the server is ruuning on http://127.0.0.1:${port}`)
+// // })
 
 
 
+
+// import React, { useEffect, useState } from 'react';
+// import './Order.css';
+// import axios from 'axios';
+
+// const Order = () => {
+//   const [orderDetails, setOrderDetails] = useState(null);
+
+//   useEffect(() => {
+//     const fetchOrderDetails = async () => {
+//       try {
+//         const response = await axios.get('API_URL');
+//         setOrderDetails(response.data);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+
+//     fetchOrderDetails();
+//   }, []);
+
+//   return (
+//     <div className="order-main">
+//       <div className="order-head">
+//         <h1>Checkout</h1>
+//       </div>
+//       <div className="payment">
+//         <span className="green-text">Payment Mode:</span>
+//         <span>Cash on Delivery</span>
+//       </div>
+//       <div className="delivery">
+//         <span className="green-text">Deliver to:</span>
+//         <span>Nagpur, Maharashtra</span>
+//       </div>
+//       <div className="total">
+//         <span className="green-text">Total:</span>
+//         <span>120Rs</span>
+//       </div>
+//       <div className="button-main">
+//         <button className="order-button">Place Order</button>
+//       </div>
+//       {orderDetails && (
+//         <div className="order-details">
+//           <h2>Order Details:</h2>
+//           {/* Display order details*/}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Order;
 
 import React, { useEffect, useState } from 'react';
 import './Order.css';
@@ -67,3 +119,5 @@ const Order = () => {
 };
 
 export default Order;
+
+// *****************************************************************
